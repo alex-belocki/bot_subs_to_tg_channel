532019b15a16ed235d773faaa3d0844c4e9f866b
Alex Belocki <abelockii80@mail.ru>
2026-02-02 19:10:31 +0300

    fix traefik error

--- services/traefik/entrypoint.prod.sh
+++ services/traefik/entrypoint.prod.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
+mkdir -p /letsencrypt
+if [ ! -f /letsencrypt/acme.json ]; then
+  touch /letsencrypt/acme.json
+fi
+chmod 600 /letsencrypt/acme.json || true
+
 exec traefik "$@"
